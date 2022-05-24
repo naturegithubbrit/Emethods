6e7815f2c65ff17e1f99a0daea9465f09b38348c
Eshwari <eshwari405yc@gmail.com>
2022-05-24 21:52:00 +0530

    test2

---
+++ src/test/java/com/automation/Test2.java
@@ -0,0 +1,15 @@
+package com.automation;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test2 {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://mvnrepository.com");
+		driver.quit();
+	}
+
+}
